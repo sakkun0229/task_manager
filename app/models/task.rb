94dcab7bc89f3d3110ebd610ba2d684f3cc7d0a9
tsakuma <dptr.tsakuma@gmail.com>
2018-03-19 13:53:54 +0900

    enum追加、form_forで正しく動くように

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :content, presence: true
-  #enum status: { NOT:0, WIP:1, DONE:2 }
-  #enum priority: { Low:0, Mid:1, High:2 }
+  enum status: { NOT:0, WIP:1, DONE:2 }
+  enum priority: { Low:0, Mid:1, High:2 }
 end
