8ef63196a9a6b8ff205e7833972ec25be06d9cab
tsakuma <dptr.tsakuma@gmail.com>
2018-03-19 13:35:10 +0900

    Task edit&updatee作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,33 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(updated_at: :desc)
+  end
+
+  def new
+    @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to tasks_path
+    if @task.save
+      redirect_to tasks_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
   end
 
   private
