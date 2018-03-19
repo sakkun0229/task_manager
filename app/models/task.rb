8514967751c2187fe011e86ed30fdd66296acc7d
tsakuma <dptr.tsakuma@gmail.com>
2018-03-19 15:42:00 +0900

    save

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,13 @@ class Task < ApplicationRecord
   validates :content, presence: true
   enum status: { NOT:0, WIP:1, DONE:2 }
   enum priority: { Low:0, Mid:1, High:2 }
+
+  def self.search(search)
+        if search
+          Task.where(["content LIKE ?", "%#{search}%"])
+        else
+          Task.all
+        end
+    end
+
 end
