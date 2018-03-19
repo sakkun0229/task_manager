eac102a57564470af23345c428c906862df933ae
tsakuma <dptr.tsakuma@gmail.com>
2018-03-19 16:15:03 +0900

    validates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, {presence: true}
+  validates :email, {presence: true, uniqueness: true}
+
+  enum admin:{false:0,admin:1}
+
 end
