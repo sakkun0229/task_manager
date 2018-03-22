e8fc73f83f8a186c5b89e6f17e153b9294e27e5e
tsakuma <dptr.tsakuma@gmail.com>
2018-03-22 13:26:26 +0900

    ログインユーザ名表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
 end
