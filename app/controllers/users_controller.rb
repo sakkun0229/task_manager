6ac2f1579aa91ac1063cf8dba8b156390ff7b080
tsakuma <dptr.tsakuma@gmail.com>
2018-03-26 15:49:06 +0900

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user,{only:[:edit,:update]}
+  before_destroy :
 
   def ensure_correct_user
     if @current_user.id != params[:id].to_i
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,5 @@
 class Label < ApplicationRecord
   has_many :task_labels
   has_many :tasks, :through => :task_labels
-
   validates :name, presence: true
 end
