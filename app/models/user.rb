c24260461331678c4282fc9a1f63929af67485e6
tsakuma <dptr.tsakuma@gmail.com>
2018-04-05 11:28:54 +0900

    before_destroyでアドミン消えないように

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :tasks, dependent: :destroy
+  before_destroy :check_admin_users, prepend: true
+
+
+  def check_admin_users
+    if User.where(admin: 'true').count == 1
+      errors.add :base, 'this user is last admin'
+      throw(:abort)
+    end
+  end
+
 
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
